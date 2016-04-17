b137d88434260327a2d96dfb3e9531290bb36158
= <=>
2016-04-16 23:46:57 -0700

    First commit, added point cloud and meshed cloud
    Former-commit-id: 3096113a1dcd0128a8c520b0554b964e52ab4881

---
+++ Assets/Scripts/SequentialSceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SequentialSceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+    //Load the next scene or go back to 0 if at end.
+    if (Input.GetKeyDown("n")) {
+      Debug.Log(SceneManager.sceneCount);
+      Debug.Log((SceneManager.GetActiveScene().buildIndex + 1) % (SceneManager.sceneCount + 1));
+      SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % (SceneManager.sceneCount + 1));
+    }
+	}
+}
