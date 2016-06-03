63d1fb468008e25cdb5644254f90f509a4c9e277
anikan <anikan33@gmail.com>
2016-06-02 18:17:42 -0700

    Added deselect button
    Former-commit-id: 945b08a2b7990f1b4c98af8d84eb326903d6bf7a

---
+++ Assets/Scripts/MonitorComponentButtonScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonitorComponentButtonScript : MonitorButtonScript {
+    public ComponentType connectedType;
+    private POIScriptComponent connectedComponent;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// Toggle if activatable.
+    /// </summary>
+    public override void AttemptToggle()
+    {
+        if (activatable)
+        {
+            connectedComponent.Toggle();
+        }
+    }
+
+    public override void OnNewNodeSelected()
+    {
+        GetComponentFromNode();
+
+        if (connectedComponent)
+        {
+            activatable = true;
+        }
+    }
+
+    public override void OnNodeDeselected()
+    {
+        activatable = false;
+    }
+
+    /// <summary>
+    /// Using the enum, determine the connected component 
+    /// </summary>
+    void GetComponentFromNode()
+    {
+        if (connectedType == ComponentType.Photos)
+        {
+            connectedComponent = Controller.selectedPOI.GetComponent<PhotoComponent>();
+        }
+
+        else if (connectedType == ComponentType.Audio)
+        {
+            connectedComponent = Controller.selectedPOI.GetComponent<AudioComponent>();
+
+        }
+
+        else if (connectedType == ComponentType.Videos)
+        {
+            connectedComponent = Controller.selectedPOI.GetComponent<VideoComponent>();
+
+        }
+
+        else if (connectedType == ComponentType.Text)
+        {
+            connectedComponent = Controller.selectedPOI.GetComponent<TextComponent>();
+
+        }
+
+        else if (connectedType == ComponentType.Scene)
+        {
+            connectedComponent = Controller.selectedPOI.GetComponent<SceneLoaderComponent>();
+
+        }
+
+        else if (connectedType == ComponentType.Zoom)
+        {
+            connectedComponent = Controller.selectedPOI.GetComponent<FocusTransformComponent>();
+
+        }
+
+        else
+        {
+            Debug.LogError("Component not setup in MonitorButtonScript");
+        }
+    } 
+}
---
+++ Assets/Scripts/MonitorDeactivateButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonitorDeactivateButtonScript : MonitorButtonScript {
+
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// Toggle if activatable.
+    /// </summary>
+    public override void AttemptToggle()
+    {
+        if (activatable)
+        {
+            Controller.selectedPOI.Deactivate();
+        }
+    }
+
+    public override void OnNewNodeSelected()
+    {
+            activatable = true;
+    }
+
+    public override void OnNodeDeselected()
+    {
+        activatable = false;
+    }
+}
