cbbc78233dc6219d317fd8f1769ac3f45ca990de
catalyst <catalyst>
2016-10-17 22:59:32 -0700

    Hackaround scene loading

--- Assets/MultiDisplay/DisplayScript.cs
+++ Assets/MultiDisplay/DisplayScript.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
 using System.Collections;
 using System.Collections.Generic;
 
 public class DisplayScript : MonoBehaviour {
 
-	private Camera[] cameras; //array of all the cameras
+	//private Camera[] cameras; //array of all the cameras
 
 	// Use this for initialization
-	void Start () {
-
+	void Awake() {
+        //SceneManager.sceneLoaded+=activateDisplays;
+        //DontDestroyOnLoad(gameObject);
+        /*
 		//searches all the children of this object for Cameras 
 		cameras = GetComponentsInChildren<Camera> ();
 
@@ -20,18 +24,47 @@ public class DisplayScript : MonoBehaviour {
 				Debug.LogError ("Error: Can't find CameraRig");
 			}
 		}
+        */
+        //Debug.LogError("displays connected: " + Display.displays.Length);
+        //      //loops through each display and activates it
+        //      int counter = 0;
+        //      Camera[] cameras = GetComponentsInChildren<Camera>();
+        //      for (int i = 0; i < cameras.Length; i++)
+        //      {
+        //          cameras[i].enabled = false;
+        //          cameras[i].enabled = true;
+        //      }			
 
-		Debug.Log("displays connected: " + Display.displays.Length);
-		//loops through each display and activates it
-		foreach (Display display in Display.displays) {
-			display.Activate ();
-		}
-			
-	}
+/*        foreach (Display display in Display.displays)
+        {
+            display.Activate();
+            //Debug.LogError("Display number: " + counter + "\n");
+            //counter++;
+        }*/
+    }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
 
+    {
+       // activateDisplays(new Scene(), LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if (Input.GetKey(KeyCode.B))
+        {
+            activateDisplays(new Scene(), LoadSceneMode.Additive);
+
+        }
+    }
+
+    void activateDisplays(Scene scenes, LoadSceneMode mode)
+    {
+        foreach (Display display in Display.displays)
+        {
+            display.Activate();
+            //Debug.LogError("Display number: " + counter + "\n");
+            //counter++;
+        }
+    }
 }
