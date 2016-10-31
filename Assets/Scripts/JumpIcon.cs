1194afaf993b796580475b55e5ecc5a6ab1c2120
catalyst <catalyst>
2016-10-30 19:50:40 -0700

    Added Kristin's Bookmark system

---
+++ Assets/Scripts/JumpIcon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpIcon : MonoBehaviour {
+
+	public GameObject icon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
