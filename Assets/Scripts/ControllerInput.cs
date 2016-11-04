34ed44408af4cb1f568cb240f778765559150dc6
Connor Smith <c3smith@ucsd.edu>
2016-11-03 20:13:26 -0700

    Added max tilt on platform.

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 
-public class ControllerInput : MonoBehaviour
-{
+public class ControllerInput : MonoBehaviour {
     public float xRotationSpeed = 30;
     public float yRotationSpeed = -30;
 
     public float forwardSpeed = 5;
+
+    [SerializeField]
+    private float maxTiltAngle = 45.0f;
 
     //Vars to Avoid duplicate actions on one press.
     //Was the A button just hit? 
@@ -33,35 +35,33 @@ public class ControllerInput : MonoBehaviour
     //Allows movement while selecting a POI using the Dpad.
     private bool advancedMode = false;
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetAxis("RightStickHorizontal") != 0)
-        {
+    void Update() {
+
+        GameObject playerShip = Controller.playerShip;
+        Transform shipTransform = playerShip.transform;
+
+
+        if(Input.GetAxis("RightStickHorizontal") != 0) {
             //Movement allowed, no POI selected or advanced mode.
-            if (Controller.selectedPOI == null || advancedMode)
-            {
+            if(Controller.selectedPOI == null || advancedMode) {
                 //Controller.playerShip.transform.Rotate(0, Time.deltaTime * Input.GetAxis("RightStickHorizontal") * xRotationSpeed, 0, Space.Self, );
-                Controller.playerShip.transform.RotateAround(Controller.instance.raycastCam.transform.parent.position, Vector3.up, Time.deltaTime * Input.GetAxis("RightStickHorizontal") * xRotationSpeed);
+                shipTransform.RotateAround(Controller.instance.raycastCam.transform.parent.position, Vector3.up, Time.deltaTime * Input.GetAxis("RightStickHorizontal") * xRotationSpeed);
             }
 
             //POI selected and not advanced mode, stick controller POI movement.
-            else if (!justActivatedRightStickHorizontal)
-            {
+            else if(!justActivatedRightStickHorizontal) {
                 justActivatedRightStickHorizontal = true;
                 //Debug.Log(Input.GetAxis("RightStickHorizontal"));
-                if (Input.GetAxis("RightStickHorizontal") > 0)
-                {
+                if(Input.GetAxis("RightStickHorizontal") > 0) {
                     pickActiveButton(1);
                 }
 
-                else
-                {
+                else {
                     pickActiveButton(-1);
                 }
 
@@ -69,146 +69,157 @@ public class ControllerInput : MonoBehaviour
         }
 
         //On release of stick, allow to be used again.
-        else if (Input.GetAxis("RightStickHorizontal") == 0 && justActivatedRightStickHorizontal)
-        {
+        else if(Input.GetAxis("RightStickHorizontal") == 0 && justActivatedRightStickHorizontal) {
             justActivatedRightStickHorizontal = false;
         }
 
-        if (Input.GetAxis("RightStickVertical") != 0)
-        {
-            if (Controller.selectedPOI == null || advancedMode)
-            {
-                Controller.playerShip.transform.Rotate(Time.deltaTime * Input.GetAxis("RightStickVertical") * yRotationSpeed, 0, 0, Space.Self);
+        if(Input.GetAxis("RightStickVertical") != 0) {
+            if(Controller.selectedPOI == null || advancedMode) {
+
+                Vector3 beforeShipRotation = shipTransform.localRotation.eulerAngles;
+
+                float rotateAngle = Time.deltaTime * Input.GetAxis("RightStickVertical") * yRotationSpeed;
+
+                shipTransform.Rotate(rotateAngle, 0, 0, Space.Self);
+
+                Vector3 afterShipRotation = shipTransform.localRotation.eulerAngles;
+
+                if(afterShipRotation.x > 180.0f) {
+
+                    afterShipRotation.x -= 360.0f;
+
+                }
+
+                if(afterShipRotation.x < -maxTiltAngle) {
+
+                    afterShipRotation.x = -maxTiltAngle;
+ 
+
+                }
+                else if (afterShipRotation.x > maxTiltAngle) {
+
+                    afterShipRotation.x = maxTiltAngle;
+
+                }
+
+                afterShipRotation.z = 0.0f;
+                shipTransform.localRotation = Quaternion.Euler(afterShipRotation);
+
+                
+
+                Debug.LogWarningFormat("New Rotation: {0}", afterShipRotation.x);
+
                 //Controller.playerShip.transform.RotateAround(raycastCam.transform.parent.transform.position, Vector3.left, Time.deltaTime * Input.GetAxis("RightStickVertical") * yRotationSpeed);
             }
         }
 
-        if (Input.GetAxis("Horizontal") != 0)
-        {
+        if(Input.GetAxis("Horizontal") != 0) {
             //Movement allowed, no POI selected or advanced mode.
-            if (Controller.selectedPOI == null || advancedMode)
-            {
+            if(Controller.selectedPOI == null || advancedMode) {
                 Controller.playerShip.transform.position += (Time.deltaTime * Input.GetAxis("Horizontal") * forwardSpeed * Controller.playerShip.transform.right);
             }
 
 
 
             //POI selected and not advanced mode, stick controller POI movement.
-            else if (!justActivatedLeftStickHorizontal)
-            {
+            else if(!justActivatedLeftStickHorizontal) {
                 justActivatedLeftStickHorizontal = true;
                 //Debug.Log(Input.GetAxis("RightStickHorizontal"));
-                if (Input.GetAxis("Horizontal") > 0)
-                {
+                if(Input.GetAxis("Horizontal") > 0) {
                     pickActiveButton(1);
                 }
 
-                else
-                {
+                else {
                     pickActiveButton(-1);
                 }
             }
         }
 
         //On release of stick, allow to be used again.
-        else if (Input.GetAxis("Horizontal") == 0 && justActivatedLeftStickHorizontal)
-        {
+        else if(Input.GetAxis("Horizontal") == 0 && justActivatedLeftStickHorizontal) {
             justActivatedLeftStickHorizontal = false;
         }
 
-        if (Input.GetAxis("Vertical") != 0)
-        {
-            if (Controller.selectedPOI == null || advancedMode)
-            {
+        if(Input.GetAxis("Vertical") != 0) {
+            if(Controller.selectedPOI == null || advancedMode) {
                 Controller.playerShip.transform.position += (Time.deltaTime * Input.GetAxis("Vertical") * forwardSpeed * Controller.playerShip.transform.forward);
             }
         }
 
-        if (Input.GetAxis("Xbox DpadX") != 0 && !justActivatedDpad)
-        {
-            if (Controller.selectedPOI != null)
-            {
+        if(Input.GetAxis("Xbox DpadX") != 0 && !justActivatedDpad) {
+            if(Controller.selectedPOI != null) {
                 pickActiveButton((int)Input.GetAxis("Xbox DpadX"));
                 justActivatedDpad = true;
             }
         }
 
         //On release of dpad, allow to be used again.
-        else if (Input.GetAxis("Xbox DpadX") == 0 && justActivatedDpad)
-        {
+        else if(Input.GetAxis("Xbox DpadX") == 0 && justActivatedDpad) {
             justActivatedDpad = false;
         }
 
         //On a press, activate
-        if (Input.GetAxis("Xbox A") != 0 && !justActivatedA)
-        {
+        if(Input.GetAxis("Xbox A") != 0 && !justActivatedA) {
+            Debug.Log("HERRO");
+
             justActivatedA = true;
 
             //If no POI selected, then try to select a new POI
-            if (Controller.selectedPOI == null)
-            {
+            if(Controller.selectedPOI == null) {
                 RaycastHit hit;
                 Physics.Raycast(Controller.instance.raycastCam.transform.position, Controller.instance.raycastCam.transform.forward, out hit, Mathf.Infinity);
 
                 Controller.inputManager.HandleHit(hit);
 
                 //If a POI was selected by this hit, we need to set the active GUI object.
-                if (Controller.selectedPOI != null)
-                {
+                if(Controller.selectedPOI != null) {
                     pickActiveButton(0);
                 }
             }
 
             //POI Selected, trigger its buttons.
-            else
-            {
+            else {
                 Controller.buttons[selectedButtonIndex].AttemptToggle();
             }
         }
 
         //On release of button, allow to be used again.
-        else if (Input.GetAxis("Xbox A") == 0)
-        {
+        else if(Input.GetAxis("Xbox A") == 0) {
             justActivatedA = false;
         }
 
         //On b press, deactvate selected POI.
-        if (Input.GetAxis("Xbox B") != 0 && !justActivatedA)
-        {
-            if (Controller.selectedPOI != null)
-            {
+        if(Input.GetAxis("Xbox B") != 0 && !justActivatedA) {
+            if(Controller.selectedPOI != null) {
                 Controller.selectedPOI.Deactivate();
             }
         }
 
         //On release of button, allow to be used again.
-        else if (Input.GetAxis("Xbox B") == 0)
-        {
+        else if(Input.GetAxis("Xbox B") == 0) {
             justActivatedB = false;
         }
 
-        if (Input.GetAxis("Xbox Start") == 1)
-        {
+        if(Input.GetAxis("Xbox Start") == 1) {
             justActivatedStart = true;
             SceneManager.LoadScene("MultiDisplayPlanet");
         }
 
-        else if (Input.GetAxis("Xbox Start") == 0)
-        {
+        else if(Input.GetAxis("Xbox Start") == 0) {
             justActivatedStart = false;
         }
 
 
         //TODO Remove
-        if (Input.GetKey(KeyCode.A))//else if (Input.GetButton("joystick button 0"))
+        if(Input.GetKey(KeyCode.A))//else if (Input.GetButton("joystick button 0"))
         {
             SceneManager.LoadScene("Mar Saba");
         }
-        if (Input.GetKey(KeyCode.S))//else if (Input.GetButton("joystick button 0"))
+        if(Input.GetKey(KeyCode.S))//else if (Input.GetButton("joystick button 0"))
         {
             SceneManager.LoadScene("Luxor");
         }
-        if (Input.GetKey(KeyCode.D))//else if (Input.GetButton("joystick button 0"))
+        if(Input.GetKey(KeyCode.D))//else if (Input.GetButton("joystick button 0"))
         {
             SceneManager.LoadScene("MultiDisplayPlanet");
         }
@@ -217,8 +228,7 @@ public class ControllerInput : MonoBehaviour
     /// <summary>
     /// Sets the selectedButtonIndex to the desired value, checking for active buttons.
     /// </summary>
-    void pickActiveButton(int offset)
-    {
+    void pickActiveButton(int offset) {
         //Saving this value to detect infinite loops.
         int originalPosition = selectedButtonIndex;
 
@@ -227,23 +237,20 @@ public class ControllerInput : MonoBehaviour
 
         //Increment the button index while the button is not active. Loops due to the mod. Guaranteed to be at least one active button, the back button.
         //Adding the length of buttons in case index is 0 and moving left. Then -1 becomes length -1, going to end. All else will be handled by mod.
-        do
-        {
+        do {
             selectedButtonIndex = (selectedButtonIndex + offset + Controller.buttons.Length) % Controller.buttons.Length;
 
             //If trying to get the first available button, offset is 0 to check current button, then 1 to check others.
-            if (offset == 0)
-            {
+            if(offset == 0) {
                 offset = 1;
             }
 
             //Emergency situation, no buttons are selected and we made a complete loop. Just return.
-            else if (originalPosition == selectedButtonIndex)
-            {
+            else if(originalPosition == selectedButtonIndex) {
                 return;
             }
         }
-        while (!Controller.buttons[selectedButtonIndex].activatable);
+        while(!Controller.buttons[selectedButtonIndex].activatable);
 
         //Select this new button.
         Controller.buttons[selectedButtonIndex].select();
