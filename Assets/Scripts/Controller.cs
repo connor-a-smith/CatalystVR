c7af2b5bff2b7b249bc574ff8d1279c150da938f
anikan <anikan33@gmail.com>
2016-11-03 23:48:20 -0700

    Initial version

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,211 +4,244 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Controller : MonoBehaviour {
-  //Is this running in the cave?
-  public static bool isCave = false;
+public class Controller : MonoBehaviour
+{
+    //Is this running in the cave?
+    public static bool isCave = false;
 
-  public static Controller instance;
+    public static Controller instance;
 
-  [SerializeField] private float cameraEyeOffset = 0.6f;
-  [SerializeField] private GameObject[] leftEyeCameras;
-  [SerializeField] private GameObject[] rightEyeCameras;
+    [SerializeField]
+    private float cameraEyeOffset = 0.6f;
+    [SerializeField]
+    private GameObject[] leftEyeCameras;
+    [SerializeField]
+    private GameObject[] rightEyeCameras;
 
-  [SerializeField] public BookmarkController bookmarks;
-  [SerializeField] public InputGuideController inputGuide;
+    [SerializeField]
+    public BookmarkController bookmarks;
+    [SerializeField]
+    public InputGuideController inputGuide;
 
-  public List<POIScript> POIList;
+    public List<POIScript> POIList;
 
-  [SerializeField] private bool is3D = false;
+    [SerializeField]
+    private bool is3D = false;
 
-  public static GameObject playerShip;
-  public static MonitorScript monitor;
-  public static InputManagerScript inputManager;
+    public static GameObject playerShip;
+    public static MonitorScript monitor;
+    public static InputManagerScript inputManager;
 
-  public static POIScript selectedPOI;
+    public static POIScript selectedPOI;
 
-  public static Material defaultPOIMat;
-  public static Material highlightedPOIMat;
-  public static Material selectedPOIMat;
+    public static Material defaultPOIMat;
+    public static Material highlightedPOIMat;
+    public static Material selectedPOIMat;
 
-  public Material defaultPOIMaterialEditor;
-  public Material highlightedPOIMaterialEditor;
-  public Material selectedPOIMaterialEditor;
+    public Material defaultPOIMaterialEditor;
+    public Material highlightedPOIMaterialEditor;
+    public Material selectedPOIMaterialEditor;
 
-  public static Object photoPrefab;
-  public Object photoPrefabEditor;
-  public Camera raycastCam;
+    public static Object photoPrefab;
+    public Object photoPrefabEditor;
+    public Camera raycastCam;
 
+    public Object labelPrefab;
 
-  public static MonitorButtonScript[] buttons;
+    public static MonitorButtonScript[] buttons;
 
-  public static string instructionText = "Select a POI or grab the sphere to move. The further the sphere from the grab point, the faster you'll go in the direction you moved it.";
+    public static string instructionText = "Select a POI.";
 
-  // Use this for initialization
-  /// <summary>
-  /// Used to set up the static variables based on the dragged in variables.
-  /// </summary>
-  void Awake() {
+    // Use this for initialization
+    /// <summary>
+    /// Used to set up the static variables based on the dragged in variables.
+    /// </summary>
+    void Awake()
+    {
 
-    if (Controller.instance == null) {
-      Controller.instance = this;
+        if (Controller.instance == null)
+        {
+            Controller.instance = this;
 
-      GameObject topLevelParent = gameObject;
+            GameObject topLevelParent = gameObject;
 
-      while (topLevelParent.transform.parent != null) {
-        topLevelParent = topLevelParent.transform.parent.gameObject;
-      }
+            while (topLevelParent.transform.parent != null)
+            {
+                topLevelParent = topLevelParent.transform.parent.gameObject;
+            }
 
-	  POIList = new List<POIScript> ();
+            POIList = new List<POIScript>();
 
-      DontDestroyOnLoad(topLevelParent);
+            DontDestroyOnLoad(topLevelParent);
 
-      playerShip = this.gameObject;
-      monitor = GetComponentInChildren<MonitorScript>();
-      inputManager = GetComponentInChildren<InputManagerScript>();
+            playerShip = this.gameObject;
+            monitor = GetComponentInChildren<MonitorScript>();
+            inputManager = GetComponentInChildren<InputManagerScript>();
 
-      defaultPOIMat = defaultPOIMaterialEditor;
-      highlightedPOIMat = highlightedPOIMaterialEditor;
-      selectedPOIMat = selectedPOIMaterialEditor;
+            defaultPOIMat = defaultPOIMaterialEditor;
+            highlightedPOIMat = highlightedPOIMaterialEditor;
+            selectedPOIMat = selectedPOIMaterialEditor;
 
-      photoPrefab = photoPrefabEditor;
+            photoPrefab = photoPrefabEditor;
 
-      buttons = monitor.GetComponentsInChildren<MonitorButtonScript>();
+            buttons = monitor.GetComponentsInChildren<MonitorButtonScript>();
 
-      for (int i = 0; i < Controller.buttons.Length; i++) {
-        Controller.buttons[i].gameObject.SetActive(false);
-      }
+            for (int i = 0; i < Controller.buttons.Length; i++)
+            {
+                Controller.buttons[i].gameObject.SetActive(false);
+            }
 
-      Controller.monitor.GetComponentInChildren<Text>().text = Controller.instructionText;
+            Controller.monitor.GetComponentInChildren<Text>().text = Controller.instructionText;
 
-      activateDisplays(new Scene(), LoadSceneMode.Additive);
+            activateDisplays(new Scene(), LoadSceneMode.Additive);
 
-      SceneManager.sceneLoaded += cleanPOI;
+            SceneManager.sceneLoaded += cleanPOI;
 
+        }
+
+        //Controller already exists, move the existing to this position and gete rid of this one.
+        else {
+            GameObject instanceObject = Controller.instance.gameObject;
+            GameObject currentObject = gameObject;
+
+            while (currentObject.transform.parent != null && instanceObject.transform.parent != null)
+            {
+                instanceObject.transform.localPosition = currentObject.transform.localPosition;
+                instanceObject.transform.localRotation = currentObject.transform.localRotation;
+                instanceObject.transform.localScale = currentObject.transform.localScale;
+
+                instanceObject = instanceObject.transform.parent.gameObject;
+                currentObject = currentObject.transform.parent.gameObject;
+
+            }
+
+            instanceObject.transform.localPosition = currentObject.transform.localPosition;
+            instanceObject.transform.localRotation = currentObject.transform.localRotation;
+            instanceObject.transform.localScale = currentObject.transform.localScale;
+
+            Destroy(currentObject);
+
+        }
     }
 
-    //Controller already exists, move the existing to this position and gete rid of this one.
-    else {
-      GameObject instanceObject = Controller.instance.gameObject;
-      GameObject currentObject = gameObject;
+    public void Start()
+    {
 
-      while (currentObject.transform.parent != null && instanceObject.transform.parent != null) {
-        instanceObject.transform.localPosition = currentObject.transform.localPosition;
-        instanceObject.transform.localRotation = currentObject.transform.localRotation;
-        instanceObject.transform.localScale = currentObject.transform.localScale;
-
-        instanceObject = instanceObject.transform.parent.gameObject;
-        currentObject = currentObject.transform.parent.gameObject;
-
-      }
-
-      instanceObject.transform.localPosition = currentObject.transform.localPosition;
-      instanceObject.transform.localRotation = currentObject.transform.localRotation;
-      instanceObject.transform.localScale = currentObject.transform.localScale;
-
-      Destroy(currentObject);
-
-    }
-  }
-
-  public void Start() {
-
-    resetCameraPositions();
-
-    monitor.GetComponentInChildren<Text>().text = Controller.instructionText;
-
-  }
-
-  // Update is called once per frame
-  void Update() {
-    if (Input.GetKey(KeyCode.B)) {
-      activateDisplays(new Scene(), LoadSceneMode.Additive);
-
-    }
-
-    if (Input.GetKeyDown(KeyCode.Alpha3)) {
-      toggle3D();
-    }
-
-    if (is3D && Input.GetKey(KeyCode.Minus)) {
-      if (cameraEyeOffset > 0) {
-
-        cameraEyeOffset -= 0.01f;
         resetCameraPositions();
-        Debug.LogErrorFormat("Camera Offset: {0}", cameraEyeOffset);
-      }
+
+        monitor.GetComponentInChildren<Text>().text = Controller.instructionText;
 
     }
 
-    if (is3D && Input.GetKey(KeyCode.Equals)) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            activateDisplays(new Scene(), LoadSceneMode.Additive);
 
-      cameraEyeOffset += 0.01f;
-      resetCameraPositions();
-      Debug.LogErrorFormat("Camera Offset: {0}", cameraEyeOffset);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            toggle3D();
+        }
+
+        if (is3D && Input.GetKey(KeyCode.Minus))
+        {
+            if (cameraEyeOffset > 0)
+            {
+
+                cameraEyeOffset -= 0.01f;
+                resetCameraPositions();
+                Debug.LogErrorFormat("Camera Offset: {0}", cameraEyeOffset);
+            }
+
+        }
+
+        if (is3D && Input.GetKey(KeyCode.Equals))
+        {
+
+            cameraEyeOffset += 0.01f;
+            resetCameraPositions();
+            Debug.LogErrorFormat("Camera Offset: {0}", cameraEyeOffset);
+
+        }
+    }
+
+
+    void activateDisplays(Scene scenes, LoadSceneMode mode)
+    {
+        foreach (Display display in Display.displays)
+        {
+            display.Activate();
+            //Debug.LogError("Display number: " + counter + "\n");
+            //counter++;
+        }
+    }
+
+    public void toggle3D()
+    {
+        if (is3D)
+        {
+            make2D();
+            return;
+        }
+
+        make3D();
+    }
+
+    public void make2D()
+    {
+        offsetCameras(leftEyeCameras, cameraEyeOffset / 2);
+        offsetCameras(rightEyeCameras, -cameraEyeOffset / 2);
+        is3D = false;
+    }
+
+    public void make3D()
+    {
+        offsetCameras(leftEyeCameras, -cameraEyeOffset / 2);
+        offsetCameras(rightEyeCameras, cameraEyeOffset / 2);
+        is3D = true;
+    }
+
+    public void offsetCameras(GameObject[] cameras, float offset)
+    {
+        foreach (GameObject camera in cameras)
+        {
+            Vector3 newPosition = camera.transform.localPosition;
+            newPosition.x += offset;
+            camera.transform.localPosition = newPosition;
+        }
+    }
+
+    public void moveCameras(GameObject[] cameras, float xVal)
+    {
+        foreach (GameObject camera in cameras)
+        {
+            Vector3 newPosition = camera.transform.localPosition;
+            newPosition.x = xVal;
+            camera.transform.localPosition = newPosition;
+        }
+    }
+
+    private void resetCameraPositions()
+    {
+        if (!is3D)
+        {
+            moveCameras(leftEyeCameras, 0.0f);
+            moveCameras(rightEyeCameras, 0.0f);
+        }
+        else {
+            moveCameras(leftEyeCameras, -cameraEyeOffset / 2);
+            moveCameras(rightEyeCameras, cameraEyeOffset / 2);
+        }
+    }
+
+    public void cleanPOI(Scene scene, LoadSceneMode mode)
+    {
+
+        POIList.Clear();
 
     }
-  }
-
-
-  void activateDisplays(Scene scenes, LoadSceneMode mode) {
-    foreach (Display display in Display.displays) {
-      display.Activate();
-      //Debug.LogError("Display number: " + counter + "\n");
-      //counter++;
-    }
-  }
-
-  public void toggle3D() {
-    if (is3D) {
-      make2D();
-      return;
-    }
-
-    make3D();
-  }
-
-  public void make2D() {
-    offsetCameras(leftEyeCameras, cameraEyeOffset / 2);
-    offsetCameras(rightEyeCameras, -cameraEyeOffset / 2);
-    is3D = false;
-  }
-
-  public void make3D() {
-    offsetCameras(leftEyeCameras, -cameraEyeOffset / 2);
-    offsetCameras(rightEyeCameras, cameraEyeOffset / 2);
-    is3D = true;
-  }
-
-  public void offsetCameras(GameObject[] cameras, float offset) {
-    foreach (GameObject camera in cameras) {
-      Vector3 newPosition = camera.transform.localPosition;
-      newPosition.x += offset;
-      camera.transform.localPosition = newPosition;
-    }
-  }
-
-  public void moveCameras(GameObject[] cameras, float xVal) {
-    foreach (GameObject camera in cameras) {
-      Vector3 newPosition = camera.transform.localPosition;
-      newPosition.x = xVal;
-      camera.transform.localPosition = newPosition;
-    }
-  }
-
-  private void resetCameraPositions() {
-    if (!is3D) {
-      moveCameras(leftEyeCameras, 0.0f);
-      moveCameras(rightEyeCameras, 0.0f);
-    }
-    else {
-      moveCameras(leftEyeCameras, -cameraEyeOffset / 2);
-      moveCameras(rightEyeCameras, cameraEyeOffset / 2);
-    }
-  }
-
-  public void cleanPOI(Scene scene, LoadSceneMode mode) {
-
-    POIList.Clear();
-
-  }
 }
--- Assets/Scripts/POIScripts/POIScript.cs
+++ Assets/Scripts/POIScripts/POIScript.cs
@@ -1,96 +1,119 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class POIScript : MonoBehaviour {
+public class POIScript : MonoBehaviour
+{
 
-  bool activated = false;
-  List<POIScriptComponent> components;
+    bool activated = false;
+    List<POIScriptComponent> components;
+    public string POIName;
+    [SerializeField] private float labelSpawnHeight = 1.5f;
 
-  // Use this for initialization
-  void Start() {
-    components = new List<POIScriptComponent>(GetComponentsInChildren<POIScriptComponent>());
-    Controller.instance.POIList.Add(this);
-  }
-
-  // Update is called once per frame
-  void Update() {
-    //No need to look at player anymore. btw messes up child objects too, such as focus positions.
-    // transform.LookAt(Controller.playerShip.transform.position);
-  }
-
-  void OnMouseDown() {
-    Toggle();
-  }
-
-  public void Activate() {
-    //If selected another node without deactivating an old one, then deactivate the old one.
-    if (Controller.selectedPOI != null && Controller.selectedPOI != this && Controller.selectedPOI) {
-      Controller.selectedPOI.Deactivate();
+    // Use this for initialization
+    void Start()
+    {
+        components = new List<POIScriptComponent>(GetComponentsInChildren<POIScriptComponent>());
+        Controller.instance.POIList.Add(this);
+        GameObject label = GameObject.Instantiate(Controller.instance.labelPrefab, this.transform, false) as GameObject;
+        Vector3 newPosition = label.transform.localPosition;
+        newPosition.y += labelSpawnHeight;
+        label.transform.localPosition = newPosition;
+        label.GetComponentInChildren<Text>().text = POIName;
     }
 
-    activated = true;
-    Controller.selectedPOI = this;
-    GetComponentInChildren<Renderer>().material = Controller.selectedPOIMat;
-        
-    //Tell all the buttons that a new poi was selected.
-    for (int i = 0; i < Controller.buttons.Length; i++) {
-      Controller.buttons[i].gameObject.SetActive(true);
-
-      Controller.buttons[i].OnNewNodeSelected();
+    // Update is called once per frame
+    void Update()
+    {
+        //No need to look at player anymore. btw messes up child objects too, such as focus positions.
+        // transform.LookAt(Controller.playerShip.transform.position);
     }
 
-    //Tell all components to activate.
-    for (int i = 0; i < components.Count; i++) {
-      if (components[i].activateImmediately) {
-        components[i].Activate();
-      }
+    void OnMouseDown()
+    {
+        Toggle();
     }
 
-  }
+    public void Activate()
+    {
+        //If selected another node without deactivating an old one, then deactivate the old one.
+        if (Controller.selectedPOI != null && Controller.selectedPOI != this && Controller.selectedPOI)
+        {
+            Controller.selectedPOI.Deactivate();
+        }
 
-  public void Deactivate() {
-    activated = false;
-    Controller.selectedPOI = null;
-    GetComponentInChildren<Renderer>().material = Controller.defaultPOIMat;
+        activated = true;
+        Controller.selectedPOI = this;
+        GetComponentInChildren<Renderer>().material = Controller.selectedPOIMat;
 
+        //Tell all the buttons that a new poi was selected.
+        for (int i = 0; i < Controller.buttons.Length; i++)
+        {
+            Controller.buttons[i].gameObject.SetActive(true);
 
-    //Tell all the buttons that there is no current POI.
-    for (int i = 0; i < Controller.buttons.Length; i++) {
-      Controller.buttons[i].OnNodeDeselected();
-      Controller.buttons[i].gameObject.SetActive(false);
+            Controller.buttons[i].OnNewNodeSelected();
+        }
+
+        //Tell all components to activate.
+        for (int i = 0; i < components.Count; i++)
+        {
+            if (components[i].activateImmediately)
+            {
+                components[i].Activate();
+            }
+        }
+
     }
 
-    for (int i = 0; i < components.Count; i++) {
-      components[i].Deactivate();
-    }
-  }
+    public void Deactivate()
+    {
+        activated = false;
+        Controller.selectedPOI = null;
+        GetComponentInChildren<Renderer>().material = Controller.defaultPOIMat;
 
-  /// <summary>
-  /// A way to toggle the state without needing to check the current state externally.
-  /// </summary>
-  public void Toggle() {
-    if (!activated) {
-      Activate();
-    }
-    else {
-      Deactivate();
-    }
-  }
 
-  /// <summary>
-  /// Highlight the POI if it is hit in the raycast.
-  /// </summary>
-  public void Highlight() {
-    GetComponentInChildren<Renderer>().material = Controller.highlightedPOIMat;
-  }
+        //Tell all the buttons that there is no current POI.
+        for (int i = 0; i < Controller.buttons.Length; i++)
+        {
+            Controller.buttons[i].OnNodeDeselected();
+            Controller.buttons[i].gameObject.SetActive(false);
+        }
 
-  /// <summary>
-  /// UnHighlight the POI if it is not hit in the raycast.
-  /// </summary>
-  public void UnHighlight() {
-    GetComponentInChildren<Renderer>().material = Controller.defaultPOIMat;
-  }
+        for (int i = 0; i < components.Count; i++)
+        {
+            components[i].Deactivate();
+        }
+    }
+
+    /// <summary>
+    /// A way to toggle the state without needing to check the current state externally.
+    /// </summary>
+    public void Toggle()
+    {
+        if (!activated)
+        {
+            Activate();
+        }
+        else {
+            Deactivate();
+        }
+    }
+
+    /// <summary>
+    /// Highlight the POI if it is hit in the raycast.
+    /// </summary>
+    public void Highlight()
+    {
+        GetComponentInChildren<Renderer>().material = Controller.highlightedPOIMat;
+    }
+
+    /// <summary>
+    /// UnHighlight the POI if it is not hit in the raycast.
+    /// </summary>
+    public void UnHighlight()
+    {
+        GetComponentInChildren<Renderer>().material = Controller.defaultPOIMat;
+    }
 }
 
-    
