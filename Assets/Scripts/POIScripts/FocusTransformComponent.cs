71282bca3e568c3a8d3be9706d67ad6a29e2d77a
catalyst <catalyst>
2016-11-05 22:05:59 -0700

    Work on focus script

--- Assets/Scripts/POIScripts/FocusTransformComponent.cs
+++ Assets/Scripts/POIScripts/FocusTransformComponent.cs
@@ -23,35 +23,16 @@ public class FocusTransformComponent : POIScriptComponent
         Quaternion oldRot = Controller.instance.raycastCam.transform.localRotation;
         Controller.instance.raycastCam.transform.LookAt(this.transform.position);
         Quaternion newRot = Controller.instance.raycastCam.transform.localRotation;
-        Debug.Log(Controller.instance.raycastCam.transform.rotation.eulerAngles + " Desired");
-
-        //Vector3 oldRot = Controller.instance.raycastCam.transform.localEulerAngles;
-        //Controller.instance.raycastCam.transform.LookAt(this.transform.position);
-        //Vector3 newRot = Controller.instance.raycastCam.transform.localEulerAngles;
 
         //Apply to platform, and reset raycastCam pos.
 
         Controller.playerShip.transform.rotation *= ((newRot * Quaternion.Inverse(oldRot)));
-        //Controller.playerShip.transform.Rotate(newRot - oldRot);
         Controller.instance.raycastCam.transform.localRotation = oldRot;
-
-        Debug.Log(Controller.instance.raycastCam.transform.rotation.eulerAngles + " Actual");
-
-
-        //Controller.playerShip.transform.position = this.transform.position - Controller.playerShip.transform.forward * 50.0f;
+        
+        //
+        Controller.playerShip.transform.position = this.transform.position - Controller.instance.raycastCam.transform.forward * 50.0f;
 
         //Teleport to location.
-
-
-        //Vector3 newRotation = Controller.playerShip.transform.rotation.eulerAngles;
-        //newRotation.z = 0;
-        //Controller.playerShip.transform.rotation = Quaternion.Euler(newRotation);
-
-        //Controller.playerShip.GetComponent<Rigidbody>().velocity = Vector3.zero;
-
-        //Lerp to location. Need to implement
-
-        //Accelerate to location. Need to implement.
     }
 
     public override void Deactivate()
